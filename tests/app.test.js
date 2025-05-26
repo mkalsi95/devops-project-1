79d49cc4829779ee8c96257810d709051ef4c72a
Mandeep <mandeep_kalsi@live.com>
2025-05-26 15:54:46 +0100

    Add GitHub Actions CI workflow

---
+++ tests/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello, DevOps!'));
+
+describe('GET /', () => {
+  it('responds with Hello, DevOps!', done => {
+    request(app)
+      .get('/')
+      .expect('Hello, DevOps!')
+      .expect(200, done);
+  });
+});
