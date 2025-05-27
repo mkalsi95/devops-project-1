2bbd0e805f9bd8df98aaaf3ad19c78c70ea430c4
Mandeep <mandeep_kalsi@live.com>
2025-05-27 16:20:17 +0100

    Add ESLint config file

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['standard'],
+  rules: {
+    // You can customize rules here
+  }
+}
